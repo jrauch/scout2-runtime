c53ffd18540a68778b6950ffab2e964b965159f8
Jeremy Rauch <jrauch@latacora.com>
2017-10-13 13:41:19 -0400

    force overwrite

--- runner.sh
+++ runner.sh
@@ -2,4 +2,4 @@
 rm /output/latest
 
 DATE=`date +%Y%m%d`
-Scout2 --no-browser --ruleset rules/mydefault.json --report-dir /output/$DATE && echo $DATE > /output/latest
+Scout2 --force --no-browser --ruleset rules/mydefault.json --report-dir /output/$DATE && echo $DATE > /output/latest
